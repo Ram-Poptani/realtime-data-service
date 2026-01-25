5eb452c4e29881c20be8acffdf541b80dead2358
Ram Poptani <911S205126@adani.com>
2026-01-25 19:40:53 +0530

    chore: Added Jackson Config

---
+++ src/main/java/org/binance/livedata/config/JacksonConfig.java
@@ -0,0 +1,23 @@
+package org.binance.livedata.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper;
+    }
+
+    @Bean
+    public JsonMapper jsonMapper() {
+        return new JsonMapper();
+    }
+}
