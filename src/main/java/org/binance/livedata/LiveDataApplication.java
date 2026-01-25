cb7c8b3e556fd0bd382bff7835be3decd729a0bb
Ram Poptani <911S205126@adani.com>
2026-01-25 19:39:04 +0530

    INIT

---
+++ src/main/java/org/binance/livedata/LiveDataApplication.java
@@ -0,0 +1,13 @@
+package org.binance.livedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/binance/livedata/LiveDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.binance.livedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
